c5d91a434cb3125a39d4e99e060ca4459b21457c
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2017-10-21 09:19:08 +0900

    Make a Python script more pythonic

--- sudokumap.py
+++ sudokumap.py
@@ -67,7 +67,7 @@ class SudokuConstAll:
 
     def get_cells_str(self, cell_str_func, length):
         str = '{'
-        str += ','.join(list(map(cell_str_func, range(0, length))))
+        str += ','.join(list(map(cell_str_func, range(length))))
         str += '}'
         return str
 
@@ -76,7 +76,7 @@ class SudokuConstAll:
         return self.get_cells_str(f, SUDOKU_COLUMNS_IN_MAP)
 
     def get_rows_str_set(self):
-        return list(map(self.get_row_str, range(0, SUDOKU_ROWS_IN_MAP)))
+        return list(map(self.get_row_str, range(SUDOKU_ROWS_IN_MAP)))
 
     def get_column_str(self, column):
         f = lambda row: self.get_cell_str(column, row)
@@ -84,29 +84,29 @@ class SudokuConstAll:
 
     def get_columns_str_set(self):
         return list(map(self.get_column_str,
-                        range(0, SUDOKU_COLUMNS_IN_MAP)))
+                        range(SUDOKU_COLUMNS_IN_MAP)))
 
     def get_box_line_str(self, x, y, ofs_x):
         f = lambda ofs_y: str(
             x + ofs_x + (y + ofs_y) * SUDOKU_COLUMNS_IN_MAP)
-        return ','.join(list(map(f, range(0, SUDOKU_BOX_VERTICAL_SIZE))))
+        return ','.join(list(map(f, range(SUDOKU_BOX_VERTICAL_SIZE))))
 
     def get_box_str_set(self, x, y):
         f = lambda ofs_x: self.get_box_line_str(
             x * SUDOKU_BOX_HORIZONTAL_SIZE, y, ofs_x)
 
         str = '{'
-        str += ','.join(list(map(f, range(0, SUDOKU_BOX_VERTICAL_SIZE))))
+        str += ','.join(list(map(f, range(SUDOKU_BOX_VERTICAL_SIZE))))
         str += '}'
         return str
 
     def get_3boxes_str_set(self, y):
         f = lambda x: self.get_box_str_set(x, y * SUDOKU_BOX_VERTICAL_SIZE)
-        return list(map(f, range(0, SUDOKU_BOX_IN_COLUMN)))
+        return list(map(f, range(SUDOKU_BOX_IN_COLUMN)))
 
     def get_boxes_str_set(self):
         return sum(list(map(self.get_3boxes_str_set,
-                            range(0, SUDOKU_BOX_IN_ROW))), [])
+                            range(SUDOKU_BOX_IN_ROW))), [])
 
     def get_reverse_group_cell_str(self, column, row):
         group = ((column // SUDOKU_BOX_IN_COLUMN)
@@ -115,21 +115,21 @@ class SudokuConstAll:
 
     def get_reverse_group_column_str(self, column):
         f = lambda row: self.get_reverse_group_cell_str(column, row)
-        return ','.join(list(map(f, range(0, SUDOKU_ROWS_IN_MAP))))
+        return ','.join(list(map(f, range(SUDOKU_ROWS_IN_MAP))))
 
     def get_reverse_group_str(self):
         str = 'const SudokuIndex SudokuMap::ReverseGroup_'
         str += "[Sudoku::SizeOfAllCells][Sudoku::SizeOfGroupsPerCell] {\n"
 
         f = lambda column: self.get_reverse_group_column_str(column)
-        str += ",\n".join(list(map(f, range(0, SUDOKU_COLUMNS_IN_MAP))))
+        str += ",\n".join(list(map(f, range(SUDOKU_COLUMNS_IN_MAP))))
         str += "\n};\n"
         return str
 
     def count_bits(self, bitmap):
         count = 0
         bitmask = 1
-        for i in range(0,SUDOKU_CELLS_IN_GROUP):
+        for i in range(SUDOKU_CELLS_IN_GROUP):
             count += 1 if (bitmap & bitmask) else 0
             bitmask *= 2
         return count
@@ -146,7 +146,7 @@ class SudokuConstAll:
         header = 'const SudokuCellLookUp SudokuCell::CellLookUp_'
         header += "[Sudoku::SizeOfLookUpCell] {\n"
         str = ','.join(list(map(self.get_element_str,
-                                range(0, 2 ** SUDOKU_CELLS_IN_GROUP))))
+                                range(2 ** SUDOKU_CELLS_IN_GROUP))))
         return header + re.sub(r'\.(,?)', "\\1\n", str) + "};"
 
 obj = SudokuConstAll()
