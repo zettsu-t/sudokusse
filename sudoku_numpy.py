841a2b8ab3569f63963937fbcf567d970cb397b7
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2018-01-02 18:34:51 +0900

    Improve the script using NumPy

--- sudoku_numpy.py
+++ sudoku_numpy.py
@@ -52,6 +52,7 @@ class SudokuSolver(object):
         '''
         Parse Sudoku puzzles in a file
         :param str filename : Name of a file that contains sudoku puzzles in each line
+        :return [nparray(9,9,9,bool)] : List of Sudoku maps
         '''
 
         sudoku_maps = []
@@ -72,7 +73,8 @@ class SudokuSolver(object):
         '''
         Parse a Sudoku puzzle
         :param str sudoku_str : 81 characters that represent a Sudoku puzzle
-        character 1..9 are preset numbers for cells and others leave cells empty.
+            character 1..9 are preset numbers for cells and others leave cells empty.
+        :return bool : Sudoku maps if sudoku_str is valid, None otherwise
         '''
 
         if len(sudoku_str) < 81:
@@ -112,28 +114,37 @@ class SudokuSolver(object):
         :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved
         '''
 
-        answer, result = self.solve_map(sudoku_map)
+        # Pass a copy of to prevent sudoku_map being overwritten
+        map_str = self.map_to_string(sudoku_map)
+        answer, result = self.solve_map(sudoku_map.copy())
         answer_str = self.map_to_string(answer)
         result = 'Solved' if self.verify(sudoku_str, answer, answer_str) else 'Not solved'
-        print(result + '\n' + answer_str)
+        print(result + '\n' + map_str + '-----------------\n' + answer_str)
         sys.stdout.flush()
 
     def solve_map(self, sudoku_map):
         '''
         Find unused candidates in rows, columns, and boxes that cells belong to
         :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved
+            This method overwrites sudoku_map.
+        :return (nparray(9,9,9,bool), bool) : (Answer of a sudoku maps,
+            whether correctly solved or not)
         '''
 
-        new_map = sudoku_map.copy()
+        new_map = sudoku_map
         count = -1
+
         while True:
             new_map = self.filter_candidates(new_map)
             new_count = np.sum(np.sum(new_map, axis=2) == 1)
             if new_count == 81:
                 return new_map, True
+
             # Inconsistent cells in backtracking
             if np.sum(np.sum(new_map, axis=2) == 0):
                 return new_map, False
+
+            # If candidates of cells do not decrease, start backtracking
             if count == new_count:
                 return self.backtrack(new_map)
             count = new_count
@@ -142,6 +153,8 @@ class SudokuSolver(object):
         '''
         Start Backtracking
         :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved
+        :return (nparray(9,9,9,bool), bool) : (Answer of a sudoku maps,
+            whether correctly solved or not)
         '''
 
         # How many candidates cells have
@@ -160,6 +173,7 @@ class SudokuSolver(object):
         if len(candidates[0]) == 0:
             return sudoku_map, False
 
+        # Guess a candidate one by one
         for candidate in candidates[0]:
             new_map = sudoku_map.copy()
             new_map[row, column, :] = False
@@ -170,80 +184,43 @@ class SudokuSolver(object):
 
         return sudoku_map, False
 
-    @staticmethod
-    def map_to_string(sudoku_map):
-        '''
-        Convert a sudoku map to a str
-        :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle
-        '''
-
-        map_str = ''
-        for row in range(0, 9):
-            cell_strs = []
-            for column in range(0, 9):
-                cell_str = ''
-                for candidate in range(0, 9):
-                    if sudoku_map[row, column, candidate]:
-                        cell_str += '{}'.format(candidate+1)
-                cell_strs.append(cell_str)
-            map_str += ','.join(cell_strs) + '\n'
-        return map_str
-
-    def verify(self, sudoku_str, answer, answer_str):
-        '''
-        Verify whether an answer is correct
-        :param str sudoku_str : 81 characters that represent a Sudoku puzzle
-        :param nparray(9,9,9,bool) answer : Solved sudoku puzzle
-        :param str answer_str : Nine lines that represent the solved sudoku puzzle
-        '''
-
-        # Each cell has only one number
-        count = np.all(np.sum(answer, axis=2) == 1)
-        # Each row has 1..9
-        row = np.all(np.sum(np.logical_or.reduce(answer, axis=0), axis=1) == 9)
-        # Each column has 1..9
-        column = np.all(np.sum(np.logical_or.reduce(answer, axis=0), axis=1) == 9)
-        # Each box has 1..9
-        box = True
-        for index in range(0, 9):
-            box &= (np.sum(np.logical_or.reduce(self.box_to_row(answer, index))) == 9)
-
-        # No preset numbers are changed
-        answer_line = ''.join(answer_str.split('\n')).replace(',', '')
-        no_change = True
-        for in_char, out_char in zip(sudoku_str, answer_line):
-            no_change &= (re.match(r'[1-9]', in_char) is None or (in_char == out_char))
-
-        result = count & row & column & box & no_change
-        return result
-
     def filter_candidates(self, sudoku_map):
         '''
         Filter candidates in cells
-        :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved
+        :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved.
+            This method overwrites sudoku_map.
         '''
 
-        new_map = sudoku_map.copy()
         for row_num in range(0, 9):
             for column_num in range(0, 9):
-                row, column, box = CELL_TO_NINE_CELLS[row_num][column_num]
-                inbox = (row % 3) * 3 + column % 3
+                self.filter_candidates_cell(sudoku_map, row_num, column_num)
+        return sudoku_map
 
-                # Filter a cell by a row, column and box the cell belongs
-                unique_set = np.full((3, 9), False, dtype=bool)
-                unused_set = np.full((3, 9), True, dtype=bool)
+    def filter_candidates_cell(self, sudoku_map, row_num, column_num):
+        '''
+        Filter candidates in a cell
+        :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved
+        :param int row_num : Row number
+        :param int column_num : Column number
+        '''
 
-                unique_set[0], unused_set[0] = self.collect_candidates(new_map[row], column)
-                unique_set[1], unused_set[1] = self.collect_candidates(np.transpose(new_map, (1, 0, 2))[column], row)
-                unique_set[2], unused_set[2] = self.collect_candidates(self.box_to_row(new_map, box), inbox)
+        row, column, box = CELL_TO_NINE_CELLS[row_num][column_num]
+        inbox = (row % 3) * 3 + column % 3
 
-                # Candidates which are not unique to other cells
-                unique_candidates = ~np.logical_or.reduce(unique_set, axis=0)
-                # Candidates which will not be used in other cells
-                unused_candidates = np.logical_or.reduce(unused_set, axis=0)
-                new_map[row_num, column_num] = np.logical_and(new_map[row_num, column_num], unique_candidates)
-                new_map[row_num, column_num] = np.logical_and(new_map[row_num, column_num], unused_candidates)
-        return new_map
+        # Filter a cell by a row, column and box the cell belongs
+        unique_set = np.full((3, 9), False, dtype=bool)
+        unused_set = np.full((3, 9), True, dtype=bool)
+
+        unique_set[0], unused_set[0] = self.collect_candidates(sudoku_map[row], column)
+        unique_set[1], unused_set[1] = self.collect_candidates(np.transpose(sudoku_map, (1, 0, 2))[column], row)
+        unique_set[2], unused_set[2] = self.collect_candidates(self.box_to_row(sudoku_map, box), inbox)
+
+        # Candidates which are not unique to other cells
+        unique_candidates = ~np.logical_or.reduce(unique_set, axis=0)
+        # Candidates which will not be used in other cells
+        unused_candidates = np.logical_or.reduce(unused_set, axis=0)
+        sudoku_map[row_num, column_num] = np.logical_and(sudoku_map[row_num, column_num], unique_candidates)
+        sudoku_map[row_num, column_num] = np.logical_and(sudoku_map[row_num, column_num], unused_candidates)
 
     @staticmethod
     def collect_candidates(nine_cells, cell_index):
@@ -251,6 +228,9 @@ class SudokuSolver(object):
         Find candidates for a cell from eight cells of a row, column or box that the cell belongs.
         :param nparray(9,bool) nine_cells : One row, column or box
         :param int cell_index : Index for a cell to be updated
+        :return (nparray(9,bool), nparray(9,bool)):
+             (candidates which are unique in other cells,
+              candidates which are unused in other cells)
         '''
 
         falses = np.full((9), False, dtype=bool)
@@ -279,20 +259,85 @@ class SudokuSolver(object):
         '''
         :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle to be solved
         :param int box_index : Index of a box in the sudoku map
+        :return nparray(9,9,bool) : candidates of nine cells in the box
         '''
 
-        # I hope there are more efficient methods to inject Sudoku maps.
         new_map = np.full((9, 9), False, dtype=bool)
         from_box_row = 3 * (box_index // 3)
         from_column = 3 * (box_index % 3)
-        for inbox in range(0, 3):
-            from_row = from_box_row + inbox
-            to_column = inbox * 3
-            new_map[to_column:to_column+3, :] = \
-                sudoku_map[from_row, from_column:from_column+3, :]
+
+        to_columns = np.arange(0, 9).reshape(3, 3)
+        from_rows = np.arange(0, 3) + from_box_row
+        new_map[to_columns, :] = sudoku_map[from_rows, from_column:from_column + 3, :]
 
         return new_map
 
+    def map_to_string(self, sudoku_map):
+        '''
+        Convert a sudoku map to a str
+        :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle
+        :return str : String which represents sudoku_map
+        '''
+
+        map_str = ''
+        for row in range(0, 9):
+            cell_strs = []
+            for column in range(0, 9):
+                cell_str = self.cell_to_string(sudoku_map, row, column)
+                cell_strs.append(cell_str)
+            map_str += ','.join(cell_strs) + '\n'
+
+        return map_str
+
+    @staticmethod
+    def cell_to_string(sudoku_map, row, column):
+        '''
+        Convert candidates of a sudoku map cell to a str
+        :param nparray(9,9,9,bool) sudoku_map : Sudoku puzzle
+        :param int row : Row number
+        :param int column : Column number
+        :return str : String which represents a cell
+        '''
+
+        # Blank cell
+        if np.sum(sudoku_map[row, column]) == 9:
+            return ' '
+
+        cell_str = ''
+        for candidate in range(0, 9):
+            if sudoku_map[row, column, candidate]:
+                cell_str += '{}'.format(candidate + 1)
+        return cell_str
+
+    def verify(self, sudoku_str, answer, answer_str):
+        '''
+        Verify whether an answer is correct
+        :param str sudoku_str : 81 characters that represent a Sudoku puzzle
+        :param nparray(9,9,9,bool) answer : Solved sudoku puzzle
+        :param str answer_str : Nine lines that represent the solved sudoku puzzle
+        :return bool : True if the answer is correct, False otherwise
+        '''
+
+        # Each cell has only one number
+        count = np.all(np.sum(answer, axis=2) == 1)
+        # Each row has 1..9
+        row = np.all(np.sum(np.logical_or.reduce(answer, axis=0), axis=1) == 9)
+        # Each column has 1..9
+        column = np.all(np.sum(np.logical_or.reduce(answer, axis=0), axis=1) == 9)
+        # Each box has 1..9
+        box = True
+        for index in range(0, 9):
+            box &= (np.sum(np.logical_or.reduce(self.box_to_row(answer, index))) == 9)
+
+        # No preset numbers are changed
+        answer_line = ''.join(answer_str.split('\n')).replace(',', '')
+        no_change = True
+        for in_char, out_char in zip(sudoku_str, answer_line):
+            no_change &= (re.match(r'[1-9]', in_char) is None or (in_char == out_char))
+
+        result = count & row & column & box & no_change
+        return result
+
 if __name__ == "__main__":
-    filename = sys.argv[1] if len(sys.argv) > 1 else 'data/sudoku_example2.txt'
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'data/sudoku_example.txt'
     SudokuSolver(filename).solve_all()
